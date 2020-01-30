08c0e6e59fbf45c29588c5bd0bc88c3bea409c98
Doug Shook <dsshook@gmail.com>
2020-01-30 13:27:54 -0600

    studio 2 starter

---
+++ app/src/main/java/com/example/studio2/ChangeActivity.kt
@@ -0,0 +1,18 @@
+package com.example.studio2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_change.*
+
+class ChangeActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/studio2/DepositFragment.kt
@@ -0,0 +1,25 @@
+package com.example.studio2
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_deposit.*
+
+class DepositFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_deposit, container, false)
+    }
+
+
+}
