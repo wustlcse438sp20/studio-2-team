58143b0707c04c09e8e5f0e3a2714df06beb48bb
KBell99 <31802760+KBell99@users.noreply.github.com>
2020-01-30 15:46:57 -0600

    lmao

--- app/src/main/java/com/example/studio2/MainActivity.kt
+++ app/src/main/java/com/example/studio2/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.studio2
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val CHANGE_ACTIVITY_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,4 +19,17 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun changeActivity(view: View){
+        val changeActivityIntent = Intent(this,ChangeActivity::class.java)
+        startActivityForResult(changeActivityIntent,CHANGE_ACTIVITY_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == CHANGE_ACTIVITY_REQUEST){
+            if(resultCode == Activity.RESULT_OK){
+
+            }
+        }
+    }
 }
